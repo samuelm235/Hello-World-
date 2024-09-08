78f76d6c974fcbeaa9ffcabbcc871a1f4ba4ac2e
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-09-08 17:29:53 +0000

    a

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("The 'ln' in the other line of code puts this on a new line!");
     }
 }
